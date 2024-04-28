a210fee718475c655369b132e4245fc1672cdb39
duydang-uit <166801948+duydang-uit@users.noreply.github.com>
2024-04-28 23:13:31 +0700

    Update train-en2any-MT.sh

--- chimera/scripts/train-en2any-MT.sh
+++ chimera/scripts/train-en2any-MT.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 # prerequisits and environment variables
+export PATH="$PATH:/kaggle/working/sample"
+export PYTHONPATH="$PYTHONPATH:/kaggle/working/sample"
+
+export MUSTC_ROOT="speech_data/mustc"
+export WMT_ROOT="wmt_data"
+export SAVE_ROOT="checkpoints"
+export target=de
 export ST_SAVE_DIR="$SAVE_ROOT/st"
 export MT_SAVE_DIR="$SAVE_ROOT/mt"
 export SAVE_DIR=$MT_SAVE_DIR
 export WAVE2VEC_DIR="$SAVE_ROOT/wave2vec"
+
 pretrained_ckpt=wav2vec_small.pt
+
 mkdir -p $ST_SAVE_DIR $MT_SAVE_DIR $WAVE2VEC_DIR $MUSTC_ROOT $WMT_ROOT
 
 # downloading wav2vec2 ckpt
