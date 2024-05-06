888e5811681370dd5b423e3ca01cadd609b047c2
duydang-uit <166801948+duydang-uit@users.noreply.github.com>
2024-05-06 18:39:16 +0700

    Update monotonic_transformer_layer.py

--- examples/simultaneous_translation/modules/monotonic_transformer_layer.py
+++ examples/simultaneous_translation/modules/monotonic_transformer_layer.py
@@ -13,7 +13,8 @@ class TransformerMonotonicEncoderLayer(TransformerEncoderLayer):
         seq_len, _, _ = x.size()
         attn_mask = x.new_ones([seq_len, seq_len]).triu(1).to(torch.float16)
         attn_mask = attn_mask.masked_fill(attn_mask.bool(), float("-inf"))
-        print(attn_mask, attn_mask.dtype)
+        print("monotonic_transformer_layer attn_mask:", attn_mask)
+        print("monotonic_transformer_layer attn_mask type:", attn_mask.dtype)
         return super().forward(x, encoder_padding_mask, attn_mask)
 
 
